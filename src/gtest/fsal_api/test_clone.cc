4b59db665c9ad4d1bd4aa8f99f85a57b928adc7a
Nilesh Somani <nsomani@cs.stonybrook.edu>
2019-03-21 13:21:14 -0400

    Simple gtest to emulate fsal_clone

---
+++ src/gtest/fsal_api/test_clone.cc
@@ -0,0 +1,197 @@
+// -*- mode:C; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Copyright (C) 2018 Red Hat, Inc.
+ * Contributor : Girjesh Rajoria <grajoria@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * -------------
+ */
+
+#include <sys/types.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <chrono>
+#include <thread>
+#include <random>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/exception.hpp>
+#include <boost/program_options.hpp>
+
+extern "C" {
+/* Manually forward this, as 9P is not C++ safe */
+void admin_halt(void);
+/* Ganesha headers */
+#include "export_mgr.h"
+#include "nfs_exports.h"
+#include "sal_data.h"
+#include "fsal.h"
+#include "common_utils.h"
+/* For MDCACHE bypass.  Use with care */
+#include "../FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_debug.h"
+}
+
+#include "gtest.hh"
+
+#define TEST_ROOT "clone"
+#define TEST_FILE "original_name"
+#define TEST_FILE_NEW "new_name"
+#define TEST_FILE_NEW_S 9 
+
+namespace {
+
+  char* ganesha_conf = nullptr;
+  char* lpath = nullptr;
+  int dlevel = -1;
+  uint16_t export_id = 77;
+  char* event_list = nullptr;
+  char* profile_out = nullptr;
+
+  class CloneTest : public gtest::GaneshaFSALBaseTest {
+  protected:
+
+    virtual void SetUp() {
+      gtest::GaneshaFSALBaseTest::SetUp();
+    }
+
+    virtual void TearDown() {
+      gtest::GaneshaFSALBaseTest::TearDown();
+    }
+  };
+
+} /* namespace */
+
+TEST_F(CloneTest, SIMPLE)
+{
+  fsal_status_t status;
+  struct fsal_obj_handle *obj = nullptr;
+  char *dst_name = nullptr;
+  char *new_name = (char *)malloc(TEST_FILE_NEW_S*sizeof(char));
+  memset(new_name, 0, TEST_FILE_NEW_S);
+  strncpy(new_name, TEST_FILE_NEW, TEST_FILE_NEW_S);
+  struct fsal_obj_handle *lookup = nullptr;
+
+  /* Create file for the the test */
+  status = fsal_create(test_root, TEST_FILE, REGULAR_FILE, &attrs, NULL, &obj,
+                  NULL);
+  ASSERT_EQ(status.major, 0);
+  ASSERT_NE(obj, nullptr);
+
+  status = test_root->obj_ops->clone(obj, &dst_name, test_root, new_name);
+  EXPECT_EQ(status.major, 0);
+  free(new_name);
+  status = test_root->obj_ops->lookup(test_root, TEST_FILE_NEW, &lookup, NULL);
+  EXPECT_EQ(status.major, 0);
+
+  /* Remove directory created while running test */
+  //status = fsal_remove(test_root, TEST_ROOT);
+  //ASSERT_EQ(status.major, 0);
+}
+
+int main(int argc, char *argv[])
+{
+  int code = 0;
+  char* session_name = NULL;
+
+  using namespace std;
+  using namespace std::literals;
+  namespace po = boost::program_options;
+
+  po::options_description opts("program options");
+  po::variables_map vm;
+
+  try {
+
+    opts.add_options()
+      ("config", po::value<string>(),
+       "path to Ganesha conf file")
+
+      ("logfile", po::value<string>(),
+       "log to the provided file path")
+
+      ("export", po::value<uint16_t>(),
+       "id of export on which to operate (must exist)")
+
+      ("debug", po::value<string>(),
+       "ganesha debug level")
+
+      ("session", po::value<string>(),
+	"LTTng session name")
+
+      ("event-list", po::value<string>(),
+	"LTTng event list, comma separated")
+
+      ("profile", po::value<string>(),
+	"Enable profiling and set output file.")
+      ;
+
+    po::variables_map::iterator vm_iter;
+    po::command_line_parser parser{argc, argv};
+    parser.options(opts).allow_unregistered();
+    po::store(parser.run(), vm);
+    po::notify(vm);
+
+    // use config vars--leaves them on the stack
+    vm_iter = vm.find("config");
+    if (vm_iter != vm.end()) {
+      ganesha_conf = (char*) vm_iter->second.as<std::string>().c_str();
+    }
+    vm_iter = vm.find("logfile");
+    if (vm_iter != vm.end()) {
+      lpath = (char*) vm_iter->second.as<std::string>().c_str();
+    }
+    vm_iter = vm.find("debug");
+    if (vm_iter != vm.end()) {
+      dlevel = ReturnLevelAscii(
+	(char*) vm_iter->second.as<std::string>().c_str());
+    }
+    vm_iter = vm.find("export");
+    if (vm_iter != vm.end()) {
+      export_id = vm_iter->second.as<uint16_t>();
+    }
+    vm_iter = vm.find("session");
+    if (vm_iter != vm.end()) {
+      session_name = (char*) vm_iter->second.as<std::string>().c_str();
+    }
+    vm_iter = vm.find("event-list");
+    if (vm_iter != vm.end()) {
+      event_list = (char*) vm_iter->second.as<std::string>().c_str();
+    }
+    vm_iter = vm.find("profile");
+    if (vm_iter != vm.end()) {
+      profile_out = (char*) vm_iter->second.as<std::string>().c_str();
+    }
+
+    ::testing::InitGoogleTest(&argc, argv);
+    gtest::env = new gtest::Environment(ganesha_conf, lpath, dlevel,
+					session_name, TEST_ROOT, export_id);
+    ::testing::AddGlobalTestEnvironment(gtest::env);
+
+    code  = RUN_ALL_TESTS();
+  }
+
+  catch(po::error& e) {
+    cout << "Error parsing opts " << e.what() << endl;
+  }
+
+  catch(...) {
+    cout << "Unhandled exception in main()" << endl;
+  }
+
+  return code;
+}
