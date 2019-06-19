7e285afa838b9243f4c5db8188a815d71c57f6e6
Akshay Aurora <akshayaurora@yahoo.com>
2019-06-18 21:42:55 -0400

    update txnlogger

--- src/include/txn_logger.h
+++ src/include/txn_logger.h
@@ -84,9 +84,14 @@ struct TxnLog {
   const char *backup_dir_path;
 };
 
-typedef struct TxnLog TxnLog;
-uint64_t create_txn_log(const db_store_t *db, const COMPOUND4args *arg,
-                        txn_context_t *txn_context);
+// convert nfs4 compounds to txnlog objects
+int nfs_vcreate_to_txnlog(const COMPOUND4args *arg, struct TxnLog *txnlog);
+
+int nfs_vwrite_to_txnlog(const COMPOUND4args *arg, struct TxnLog *txnlog);
+
+int nfs_vunlink_to_txnlog(const COMPOUND4args *arg, struct TxnLog *txnlog);
+
+int nfs_vrename_to_txnlog(const COMPOUND4args *arg, struct TxnLog *txnlog);
 
 #ifdef __cplusplus
 }
